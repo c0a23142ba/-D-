e75e049362d05a1e5e7573d7b8f416b87366f608
Mano Akiha <c0a23142ba@edu.teu.ac.jp>
2024-04-16 18:37:56 +0900

    演習1

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -13,18 +13,37 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False) #練習7-1
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img,True,False) #練習3
+    kk_rct = kk_img.get_rect()#練習8-1
+    kk_rct.center = 300 , 200 #練習8-2
     tmr = 0
+    x = 0
+    y = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            key_lst =pg.key.get_pressed()
+        x,y = 0,0
+        if key_lst[pg.K_UP]:
+            y -= 1
+        if key_lst[pg.K_DOWN]:
+            y += 1
+        if key_lst[pg.K_LEFT]:
+            x -=1
+        if key_lst[pg.K_RIGHT]:
+            x += 3
+        if key_lst[pg.K_UP]  or key_lst[pg.K_DOWN] or key_lst[pg.K_LEFT] == False or key_lst[pg.K_RIGHT] == False:
+            x -=1
+        kk_rct.move_ip(x,y) 
+            
+
         x = tmr%3200
     
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2,[-x+1600,0])#練習7-1
         screen.blit(bg_img, [-x+3200, 0])#練習7-2
         screen.blit(bg_img2,[-x+4800,0])#練習7-2
-        screen.blit(kk_img,[300,200]) #練習4
+        screen.blit(kk_img,kk_rct) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(200)
